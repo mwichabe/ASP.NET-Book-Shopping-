9a52e38e010fb235d0aeae74bbb115b509e5809c
mwichabe <mwichabecollins@gmail.com>
2024-05-27 20:22:54 +0300

    All

---
+++ bookStore/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using bookStore.Models;
+
+namespace bookStore.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Products()
+    {
+    
+        return View();
+    }
+
+    public IActionResult Carts()
+    {
+
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
